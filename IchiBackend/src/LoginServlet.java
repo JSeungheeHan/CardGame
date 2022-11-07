b6da389385347659371ba46c6475b629dc7c0fca
Levi Pinkert <pinkert@usc.edu>
2022-11-06 17:32:26 -0800

    sample servlet code

--- IchiBackend/src/LoginServlet.java
+++ IchiBackend/src/LoginServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.util.stream.Collectors;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,10 +17,21 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * This sample code just shows how to get info off of a request and send a response
+	 * It doesn't actually verify the request against the database
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Not yet implemented. Seems like the server is working, though!");
+		//Parse the input
+		String input = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+		String[] strs = input.split(" ");
+		String username = strs[0];
+		String password = strs[1];
+		
+		//TODO: make a jdbc call and see if this exists. for now, just verify if the username is levi-pinkert and the password is password1111
+		boolean result = username.equals("levi-pinkert") && password.equals("password1111");
+		
+		//Send a response
+		response.getWriter().append(result ? "Success" : "Failure");
 	}
 
 }
