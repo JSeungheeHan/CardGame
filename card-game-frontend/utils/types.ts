508a392dfc403510eb1feefd8ffd170d41431cae
Levi Pinkert <pinkert@usc.edu>
2022-11-15 12:37:53 -0800

    fixed drawing on frontend

--- card-game-frontend/utils/types.ts
+++ card-game-frontend/utils/types.ts
@@ -17,7 +17,7 @@ export interface PlayerInfo {
 
 export interface CardInfo {
     id: string;
-    face: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "reverse" | "swap" | "shuffle" | "wild" | "bomb";
+    face: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "reverse" | "swap" | "shuffle" | "wild" | "bomb" | "deck";
     color: "red" | "green" | "blue" | "yellow" | "special";
     revealed: boolean;
     selectable: boolean;
